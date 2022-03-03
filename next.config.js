7617cc3e3f0b18c0c975aac492901b78371ffc5a
eeshaan <eeshaan@cs.wisc.edu>
2022-03-02 22:42:54 -0600

    add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback.fs = false;
@@ -12,4 +16,4 @@ module.exports = {
     path: ""
   },
   swcMinify: true
-};
+});
