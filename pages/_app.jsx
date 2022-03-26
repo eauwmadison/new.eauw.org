17e87488192021d7a3cd1eb7f273515f1491d2a3
David Viggiano <91092410+dviggiano@users.noreply.github.com>
2022-03-25 19:05:19 -0500

    add Google analytics to dependencies and _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,9 @@
 import "../styles/main.scss";
 import { CloudCannonConnect } from "@cloudcannon/react-connector";
+import ReactGA from 'react-ga';
+
+const PROPERTY_ID = "308458331";
+ReactGA.initialize(PROPERTY_ID);
 
 export default function App({ Component, pageProps }) {
   const AppComponent = CloudCannonConnect(Component);
