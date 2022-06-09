c77afe965bfc3e6e2ea2ec58535e6d2cb6408c18
eeshaan <me@eeshaan.life>
2022-06-08 23:49:09 -0500

    add Google Calendar link and tooltip

--- components/event.jsx
+++ components/event.jsx
@@ -1,3 +1,5 @@
+import ReactTooltip from "react-tooltip";
+
 import Icon from "./icon";
 
 export default function Event({ event }) {
@@ -5,26 +7,47 @@ export default function Event({ event }) {
   const eventEnd = new Date(event.end.dateTime);
 
   return (
-    <div className="event hover">
-      <div className="event-header">
-        <span className="event-title">{event.summary}</span>
-        <div className="event-info">
-          <span className="event-date">
-            <Icon icon="Date" />
-            {eventStart.toLocaleDateString(undefined, {
-              month: "long",
-              day: "numeric",
-              year: "numeric"
-            })}
-          </span>
+    <a
+      className="event-link"
+      href={event.htmlLink}
+      target="_blank"
+      rel="noreferrer"
+      data-tip="View in Google Calendar"
+    >
+      <div className="event hover">
+        <div className="event-header">
+          <span className="event-title">{event.summary}</span>
+          <div className="event-info">
+            <span className="event-date">
+              <Icon icon="Date" />
+              {eventStart.toLocaleDateString(undefined, {
+                month: "long",
+                day: "numeric",
+                year: "numeric"
+              })}
+            </span>
+          </div>
+        </div>
+        <div className="event-description">
+          {event.description || "No description"}
+        </div>
+        <div className="event-type">
+          <span>Social</span>
         </div>
       </div>
-      <div className="event-description">
-        {event.description || "No description"}
-      </div>
-      <div className="event-type">
-        <span>Social</span>
-      </div>
-    </div>
+      <ReactTooltip
+        place="right"
+        className="event-tooltip"
+        border={true}
+        getContent={(dataTip) => {
+          return (
+            <span className="event-tooltip-child">
+              {dataTip}
+              <Icon icon="Link" />
+            </span>
+          );
+        }}
+      />
+    </a>
   );
 }
